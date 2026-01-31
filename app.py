ef0676104143d5f5de745313323bb227d72bd97b
NAGARAJUHL02 <nagarajuhl0007@gmail.com>
2026-01-31 15:18:26 +0530

    flask

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/<name>")
-def name(name):
-    return render_template("name.html", name=name)
-
 @app.route("/abc")
 def abc():
     return render_template("abc.html")
+
+@app.route("/<name>")
+def name(name):
+    return render_template("name.html", name=name)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
